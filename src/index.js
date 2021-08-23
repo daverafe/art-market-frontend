28ca3e8f03881f11ea249cd96c9a1a01f41bad81
David Rafe <david.rafe@gmail.com>
2021-08-23 17:07:46 -0400

    set up configuration for redux store and default reducer

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux' 
+import {Provider} from 'react-redux' 
+import manageArt from './reducers/manageArt';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(manageArt, composeWithDevTools())
 
 ReactDOM.render(
   <React.StrictMode>
+  <Provider store={store}>
     <App />
+  </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/reducers/manageArt.js
@@ -0,0 +1,9 @@
+const manageArt = (state = {
+    users: [],
+    art_posts: [],
+    cart: [],
+}, action) => {
+    return state 
+}
+
+export default manageArt
