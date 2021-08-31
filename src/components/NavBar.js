4b3e3a56b75854f374fd46e627ea57ba574d0b06
David Rafe <david.rafe@gmail.com>
2021-08-31 19:17:44 -0400

    active storage working for image upload

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap'
-import {useSelector} from 'react-redux'
+// import {useSelector} from 'react-redux'
 import {Link} from 'react-router-dom'
 
 function NavBar() {
 
-    const users = useSelector(state => state.users.users)
-    const currentUser = users.find(user => user.jwt)
+    // const users = useSelector(state => state.users.users)
+    // const currentUser = users.find(user => user.jwt)
     
     return (
         <>
@@ -15,8 +15,7 @@ function NavBar() {
                 <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link as={Link} to="/art_posts">Art</Nav.Link>
-                    { currentUser ?
-                    <Nav.Link as={Link} to="/art_posts/new">Create Art Post</Nav.Link> : null}
+                    <Nav.Link as={Link} to="/art_posts/new">Create Art Post</Nav.Link>
                     <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link>
                     <Nav.Link as={Link} to="/login">Login</Nav.Link>
                     <Nav.Link as={Link} to="/cart">Cart</Nav.Link>
